69c04b241b58f74351e1b9a33e12d5311d3afcda
atmunr <atmunr@gmail.com>
2020-03-31 02:29:04 -0300

    Change tests for OLS
    Based on data from the original univar program.

--- R/fitSimpleLinearRegressionOLS.R
+++ R/fitSimpleLinearRegressionOLS.R
@@ -16,8 +16,7 @@ fitSimpleLinearRegressionOLS <- function (x, y) {
 	s_A <- s_E / sqrt(sum(xc ^ 2))
 	s_B <- s_E * sqrt((1 / length(x)) + ((mean(x) ^ 2) / sum(xc ^ 2)))
 
-	t <- qt(.025, df = length(x) - 2)
+	t <- qt(.975, df = length(x) - 2)
 
 	return (list( A, s_A, t*s_A, B, s_B, t*s_B, E, s_E ))
 }
-
--- tests/testthat/test_fitSimpleLinearRegressionOLS.R
+++ tests/testthat/test_fitSimpleLinearRegressionOLS.R
@@ -2,19 +2,24 @@ context("fitSimpleLinearRegressionOLS")
 
 test_that("Line is fit and other values are computed correctly", {
 
-	analytes <- c(0, 1, 2, 3,  4,  5)
-	signals  <- c(2, 4, 6, 7, 10, 12)
+	calib.data <- data.matrix(read.csv('calibration_data.txt',
+		header = FALSE, sep = ''))
+
+	analytes <- calib.data[, 1]
+	signals  <- calib.data[, 2]
+	for (i in 3 : ncol(calib.data)) {
+		analytes <- c(analytes, calib.data[, 1])
+		signals <- c(signals, calib.data[, i])
+	}
 
 	a <- fitSimpleLinearRegressionOLS(analytes, signals)
-	tol <- 1e-7
+	tol <- 1e-3
 
-	expect_equal(a[[1]], 1.9714286, tolerance = tol)
-	expect_equal(a[[2]], 0.10816968, tolerance = tol)
-	expect_equal(a[[3]], -0.30032719, tolerance = tol)
-	expect_equal(a[[4]], 1.9047619, tolerance = tol)
-	expect_equal(a[[5]], 0.32749998, tolerance = tol)
-	expect_equal(a[[6]], -0.90928571, tolerance = tol)
-	expect_equal(a[[7]], tolerance = tol,
-		c(-0.095238095, -0.123809524, -0.152380952, 0.819047619, -0.209523810, -0.238095238))
-	expect_equal(a[[8]], 0.45250625, tolerance = tol)
+	expect_equal(a[[1]], 1.3174, tolerance = tol)
+	expect_equal(a[[2]], 0.014243, tolerance = tol)
+	expect_equal(a[[3]], 0.030201, tolerance = tol)
+	expect_equal(a[[4]], 0.12365, tolerance = tol)
+	expect_equal(a[[5]], 0.043124, tolerance = tol)
+	expect_equal(a[[6]], 0.091455, tolerance = tol)
+	expect_equal(a[[8]], 0.1032, tolerance = tol)
 })
