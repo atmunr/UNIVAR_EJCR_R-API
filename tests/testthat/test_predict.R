4128ec429a5f3ce89f61078aa1358b227843e872
atmunr <atmunr@gmail.com>
2020-04-04 18:06:39 -0300

    Add test for estimateUncertaintyOfPredictedValues()

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -1,5 +1,7 @@
+library("zeallot")
 context("fitSimpleLinearRegressionOLS")
 context("predict.R")
+context("utils.R")
 
 test_that("The analyte concentrations are predicted correctly", {
 
@@ -26,4 +28,35 @@ test_that("The analyte concentrations are predicted correctly", {
 	tol <- 1e-3
 	expect_equal(predictAnalyteConcentrations(test.data, slope, intercept),
 		c(.4349502, 1.5203981, 2.5577725, 3.5065905), tolerance = tol)
+})
+
+test_that("The uncertainty of predicted responses is estimated correctly", {
+
+	# Calibration
+	calib.data <- data.matrix(read.csv('calibration_data.txt',
+		header = FALSE, sep = ''))
+
+	c(analytes, signals) %<-% createDataPoints(calib.data)
+
+	a <- fitSimpleLinearRegressionOLS(analytes, signals)
+	slope <- a[[1]]
+	intercept <- a[[4]]
+	gamma <- a[[9]]
+
+	# Predict analyte concentrations
+	test.data <- data.matrix(read.csv('test_data.txt',
+		header = FALSE, sep = ''))
+
+	predicted <- predictAnalyteConcentrations(test.data, slope, intercept)
+
+	# Estimate uncertainty
+	c(devstud, dev, reldev) %<-% estimateUncertaintyOfPredictedValues(
+		predicted, test.data, analytes, signals, gamma, slope)
+	devstud <- abs(devstud)
+
+	# Testing
+	tol <- 1e-3
+	expect_equal(devstud, c(  .113910,  .106010,  .103610,  .106140), tolerance = tol)
+	expect_equal(dev    , c(  .053712,  .049986,  .048856,  .050049), tolerance = tol)
+	expect_equal(reldev , c(12.348900, 3.287700, 1.910100, 1.427300), tolerance = tol)
 })
