92345455982d568dacf7a487b65de9a585135efa
atmunr <atmunr@gmail.com>
2020-04-11 22:31:50 -0300

    Rewrite test_predict.R to make it prettier
    Replace double quotes (") with single quotes ('), use zealot list
    unpacking where it can be used, and do all of the set up for running the
    tests outside of the test_that() functions calls. This makes the code
    less redundant.

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -1,61 +1,40 @@
-library("zeallot")
-context("fitSimpleLinearRegressionOLS")
-context("predict.R")
-context("utils.R")
+library('zeallot')
+context('fitSimpleLinearRegressionOLS')
+context('predict.R')
+context('utils.R')
 
-test_that("The analyte concentrations are predicted correctly", {
+# Calibration
+calib_data <- data.matrix(read.csv('calibration_data.txt',
+	header = FALSE, sep = ''))
 
-	# Calibration
-	calib.data <- data.matrix(read.csv('calibration_data.txt',
-		header = FALSE, sep = ''))
+c(analytes, signals) %<-% createDataPoints(calib_data)
 
-	analytes <- calib.data[, 1]
-	signals  <- calib.data[, 2]
-	for (i in 3 : ncol(calib.data)) {
-		analytes <- c(analytes, calib.data[, 1])
-		signals <- c(signals, calib.data[, i])
-	}
+c(
+	slope, ., .,
+	intercept, ., ., ., .,
+	gamma, ...
+) %<-% fitSimpleLinearRegressionOLS(analytes, signals)
 
-	# Prediction
-	a <- fitSimpleLinearRegressionOLS(analytes, signals)
-	slope <- a[[1]]
-	intercept <- a[[4]]
+# Prediction
+test.data <- data.matrix(read.csv('test_data.txt',
+	header = FALSE, sep = ''))
 
-	test.data <- data.matrix(read.csv('test_data.txt',
-		header = FALSE, sep = ''))
-	
-	# Testing
-	tol <- 1e-3
+predicted <- predictAnalyteConcentrations(test.data, slope, intercept)
+
+# Estimating uncertainty
+c(devstud, dev, reldev) %<-% estimateUncertaintyOfPredictedValues(
+	predicted, test.data, analytes, signals, gamma, slope)
+devstud <- abs(devstud)
+
+# Testing
+tol <- 1e-3
+
+test_that('The analyte concentrations are predicted correctly', {
 	expect_equal(predictAnalyteConcentrations(test.data, slope, intercept),
 		c(.4349502, 1.5203981, 2.5577725, 3.5065905), tolerance = tol)
 })
 
-test_that("The uncertainty of predicted responses is estimated correctly", {
-
-	# Calibration
-	calib.data <- data.matrix(read.csv('calibration_data.txt',
-		header = FALSE, sep = ''))
-
-	c(analytes, signals) %<-% createDataPoints(calib.data)
-
-	a <- fitSimpleLinearRegressionOLS(analytes, signals)
-	slope <- a[[1]]
-	intercept <- a[[4]]
-	gamma <- a[[9]]
-
-	# Predict analyte concentrations
-	test.data <- data.matrix(read.csv('test_data.txt',
-		header = FALSE, sep = ''))
-
-	predicted <- predictAnalyteConcentrations(test.data, slope, intercept)
-
-	# Estimate uncertainty
-	c(devstud, dev, reldev) %<-% estimateUncertaintyOfPredictedValues(
-		predicted, test.data, analytes, signals, gamma, slope)
-	devstud <- abs(devstud)
-
-	# Testing
-	tol <- 1e-3
+test_that('The uncertainty of predicted responses is estimated correctly', {
 	expect_equal(devstud, c(  .113910,  .106010,  .103610,  .106140), tolerance = tol)
 	expect_equal(dev    , c(  .053712,  .049986,  .048856,  .050049), tolerance = tol)
 	expect_equal(reldev , c(12.348900, 3.287700, 1.910100, 1.427300), tolerance = tol)
