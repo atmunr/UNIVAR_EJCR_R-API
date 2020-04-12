b868774965383f3870efbc1d7743143b20ecbf31
atmunr <atmunr@gmail.com>
2020-04-11 22:53:09 -0300

    Make test_ftest.R prettier

--- tests/testthat/test_ftest.R
+++ tests/testthat/test_ftest.R
@@ -1,18 +1,24 @@
-context("ftest.R")
+context('ftest.R')
 
-test_that("F-test figures look right", {
+test_that('F-test figures look right', {
 
 	samples <- data.matrix(read.csv('calibration_data.txt',
 		header = FALSE, sep = ''))[, -1]
 	devres <- 0.1032031
 
-	a <- runFTest(samples, devres)
-	tol <- 1e-3
+	c(
+		noise,
+		fexpect,
+		critfvalue,
+		cumuldist,
+		pass
+	) %<-% runFTest(samples, devres)
 
-	expect_equal(a[[1]],  .08212457, tolerance = tol)
-	expect_equal(a[[2]], 1.57920800, tolerance = tol)
-	expect_equal(a[[3]], 2.59888100, tolerance = tol)
-	expect_equal(a[[4]],  .21400000, tolerance = tol)
-	expect_equal(a[[5]], TRUE)
+	tol <- 1e-3
+	expect_equal(noise     ,  .08212457, tolerance = tol)
+	expect_equal(fexpect   , 1.57920800, tolerance = tol)
+	expect_equal(critfvalue, 2.59888100, tolerance = tol)
+	expect_equal(cumuldist ,  .21400000, tolerance = tol)
+	expect_equal(pass, TRUE)
 })
 
